70a0ee982b4d636f5c704d9b5e56d3d82692ceeb
Matthew Kirkendall <kirk.viola@gmail.com>
2021-10-22 15:49:17 -0400

    Added a couple monster classes

---
+++ DragonQuest/Dragon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonQuest
+{
+    class Dragon : Monster
+    {
+        public Dragon()
+        {
+            this.Name = "Dragon";
+            this.Strength = 12;
+            this.HealthPoints = 100;
+        }
+    }
+}
---
+++ DragonQuest/Spider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonQuest
+{
+    class Spider : Monster
+    {
+        public Spider()
+        {
+            this.Name = "Spider";
+            this.Strength = 5;
+            this.HealthPoints = 20;
+        }
+    }
+}
